13b4ae24922fc5da7318e5b87694d94240e22523
Mohammed Sadiq <sadiqmmm@gmail.com>
2018-02-09 02:52:09 +0530

    Add document work model

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,9 +1,50 @@
-import {Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
 	selector: 'documents',
 	templateUrl: 'documents.component.html'
 })
 export class DocumentsComponent {
-	
+	documents: Document[] = [
+		{
+			title: "My first Document",
+			description: "Great Docs",
+			file_url: "http://google.com",
+			updated_at: "11/11/18",
+			image_url: "http://google.com",
+		},	
+
+		{
+			title: "My 2 Document",
+			description: "Great Docs",
+			file_url: "http://google.com",
+			updated_at: "11/11/18",
+			image_url: "http://google.com",
+		},	
+
+		{
+			title: "My 3 Document",
+			description: "Great Docs",
+			file_url: "http://google.com",
+			updated_at: "11/11/18",
+			image_url: "http://google.com",
+		},	
+
+		{
+			title: "My 4 Document",
+			description: "Great Docs",
+			file_url: "http://google.com",
+			updated_at: "11/11/18",
+			image_url: "http://google.com",
+		},	
+
+		{
+			title: "My 5 Document",
+			description: "Great Docs",
+			file_url: "http://google.com",
+			updated_at: "11/11/18",
+			image_url: "http://google.com",
+		},	
+	]
 }
